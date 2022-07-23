b300c8b329088eadadb36c5aef6b759434cb7fd1
Niraj Pradhan <niraj.pradhan.393@gmail.com>
2022-07-23 10:15:03 +0545

    update: files directory structure

--- src/pages/api/data.js
+++ src/pages/api/data.js
@@ -2,7 +2,7 @@
 
 export default function handler(req, res) {
   res.status(200).json({
-    name: "Next app test",
+    name: "next-api-host-test",
     fetched: "Static Props",
     host: req.headers.host,
     date: new Date(),
